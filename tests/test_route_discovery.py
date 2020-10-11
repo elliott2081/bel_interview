628960694a24a5f63c9a572d8659a851f52deca1
“David <elliott2081@gmail.com>
2020-10-11 08:34:06 +0000

    Added tests for the endpoints

--- tests/test_route_discovery.py
+++ tests/test_route_discovery.py
@@ -1,6 +1,6 @@
 
 import pytest
-
+import json
 
 @pytest.fixture
 def expected_routes():
@@ -20,6 +20,40 @@ def expected_routes():
         },
     }
 
+def test_index(app, client):
+    """test that / path has expected params"""
+    res = client.get('/')
+    assert res.status_code == 200
+    expected = {'mean': '/mean', 'median': '/median', 'spectrum': '/spectrum'}
+    assert expected == json.loads(res.get_data(as_text=True))
+
+def test_mean(app, client):
+    """test that /mean path has expected params"""
+    res = client.get('/mean')
+    assert res.status_code == 200
+    expected = {'params': ['dtype']}
+    assert expected == json.loads(res.get_data(as_text=True))
+
+def test_median(app, client):
+    """test that /median path has expected params"""
+    res = client.get('/median')
+    assert res.status_code == 200
+    expected = {'params': ['dtype']}
+    assert expected == json.loads(res.get_data(as_text=True))
+
+def test_median_post(app, client):
+    """test that /median POST has expected values"""
+    res = client.post('/median', json={"dtype": "uint8", "spectrum": "{1,2,3,900===}"})
+    assert res.status_code ==200
+    expected = {'median': 213.0}
+    assert expected == json.loads(res.get_data(as_text=True))
+
+def test_spectrum(app, client):
+    """test that /spectrum path has expected params"""
+    res = client.get('/spectrum')
+    assert res.status_code == 200
+    expected = {"params": ["sampling_rate", "units", "dtype"]}
+    assert expected == json.loads(res.get_data(as_text=True))
 
 def test_routes_are_available(client, app, expected_routes):
     """test that all routes are available"""
@@ -28,7 +62,6 @@ def test_routes_are_available(client, app, expected_routes):
     routes = response.get_json()
     assert len(routes) == len(expected_routes)
     assert all(route in routes for route in expected_routes)
-
 
 def test_routes_have_right_methods(client, app, expected_routes):
     """test that the routes allow the correct methods"""
@@ -39,7 +72,6 @@ def test_routes_have_right_methods(client, app, expected_routes):
         methods = response.headers.get('Allow')
         methods = { m.strip() for m in methods.split(',') }
         assert expected_methods == methods
-
 
 def test_routes_require_right_methods(client, app, expected_routes):
     """test that the routes allow the correct methods"""
